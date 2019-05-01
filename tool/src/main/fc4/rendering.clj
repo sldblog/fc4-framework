db651f1807675f9caf5aa20e7ead496fdda12633
Avi Flax <avi.flax@fundingcircle.com>
2019-05-01 12:34:54 -0400

    [tool] Update slightly out-of-date docstring
    Another change I should have made as part of 6d3e3df0 but forgot 🤦‍♂️.

--- tool/src/main/fc4/rendering.clj
+++ tool/src/main/fc4/rendering.clj
@@ -10,6 +10,7 @@
   "A potentially resource-intensive abstraction that can render Structurizr
   Express diagrams. Implementations must also implement java.io.Closeable."
   (render [renderer diagram-yaml]
-    "Start must be called first. diagram-yaml must be a string containing the YAML source of a
-    Structurizr Express diagram. Currently no implementations support concurrent calls to render.
-    Blocks. Returns either ::success-result or ::failure-result."))
+    "diagram-yaml must be a string containing the YAML source of a Structurizr
+    Express diagram. Implementations may or may not support concurrent calls to
+    render; see their docs. Blocks. Returns either ::success-result or
+    ::failure-result."))
